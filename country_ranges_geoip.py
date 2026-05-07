2cf1f57d447d02f373be9bf67b2f86f167ec8e62
iwik <iwik@iwik.org>
2026-05-07 23:07:23 +0200

    optimize update process & added github workflow to generate ranges

--- country_ranges_geoip.py
+++ country_ranges_geoip.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# This script proceses data from GeoIP-legacy.csv built by https://mailfud.org/geoip-legacy
+# This script proceses data from GeoIP-legacy.csv built by
+# https://mailfud.org/geoip-legacy
 # https://mailfud.org/geoip-legacy/GeoIP-legacy.csv.gz
 
 from ipaddress import ip_address, IPv4Address, IPv6Address, IPv4Network, IPv6Network
@@ -8,55 +9,77 @@ import ipaddress
 import csv
 import time
 import os
+import requests
+import gzip
+import io
 
 data=[]
 country=[]
 
 header = "# IP address data are based on MaxMind GeoLite2 data released under https://www.maxmind.com/en/geolite2/eula ; generated at %s @ %s\n" % (time.strftime("%Y-%m-%d %H:%M"), os.uname().nodename)
 
-def IpaddrType(IP: str) -> str: 
-    try: 
+def IpaddrType(IP: str) -> str:
+    try:
         return "IPv4" if type(ip_address(IP)) is IPv4Address else "IPv6"
-    except ValueError: 
+    except ValueError:
         return "Invalid"
 
-with open('GeoIP-legacy.csv', mode ='r') as file:
-  csvFile = csv.DictReader(file)
-  for line in csvFile:   
-       if IpaddrType(line['start_ip']) == "IPv4" :
-            start_ip=ipaddress.IPv4Address(line['start_ip'])
-            end_ip=ipaddress.IPv4Address(line['end_ip'])
-            cidr =[ipaddr for ipaddr in ipaddress.summarize_address_range(start_ip, end_ip)]
+url = "https://mailfud.org/geoip-legacy/GeoIP-legacy.csv.gz"
 
-       else:
-            start_ip=ipaddress.IPv6Address(line['start_ip'])
-            end_ip=ipaddress.IPv6Address(line['end_ip'])
-            cidr =[ipaddr for ipaddr in ipaddress.summarize_address_range(start_ip, end_ip)]
-          
-       data.append([line['country'],cidr])
+response = requests.get(url)
+response.raise_for_status()
+
+for start_ip, end_ip, _, _, country_code, _ in csv.reader(
+    line.decode('utf-8') for line in gzip.GzipFile(fileobj=io.BytesIO(response.content))
+):
+
+    if IpaddrType(start_ip) == "IPv4" :
+
+        start_ip=ipaddress.IPv4Address(start_ip)
+        end_ip=ipaddress.IPv4Address(end_ip)
+
+        cidr =[ipaddr for ipaddr in ipaddress.summarize_address_range(start_ip, end_ip)]
+
+    else:
+
+        start_ip=ipaddress.IPv6Address(start_ip)
+        end_ip=ipaddress.IPv6Address(end_ip)
+
+        cidr =[ipaddr for ipaddr in ipaddress.summarize_address_range(start_ip, end_ip)]
+
+    data.append([country_code,cidr])
 
 for line in data:
     country.append(line[0])
-    
+
 countries=list(set(country))
 
 # Remove special country codes
 # https://dev.maxmind.com/geoip/whats-new-in-geoip2#custom-country-codes
-countries.remove("EU")
+
+if "EU" in countries:
+    countries.remove("EU")
 
 for country in countries:
+
     print (country)
+
     with open("%s.cidr" % country, 'w') as ipv4, open("%s.ipv6" % country, 'w') as ipv6 :
-        
+
         ipv4.write(header)
         ipv6.write(header)
+
         for line in data:
+
             if line[0] == country:
+
                 for cidr in line[1]:
+
                     if type(cidr) == IPv4Network:
-                            ipv4.write("%s\n" % (cidr))
+                        ipv4.write("%s\n" % (cidr))
+
                     if type(cidr) == IPv6Network:
-                            ipv6.write("%s\n" % (cidr))
-                    #print(line)
+                        ipv6.write("%s\n" % (cidr))
+
         ipv4.close()
         ipv6.close()
